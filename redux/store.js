c457356c4b9e86c51a4d9a716cec0abda1205e72
Miro Dojkic <mdojkic@gmail.com>
2017-09-29 11:51:47 +0200

    remove logger

--- redux/store.js
+++ redux/store.js
@@ -1,5 +1,4 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import races from './modules/races';
 
