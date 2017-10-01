83e592ed792ca9a0b01cd1e7127427b5bbd7afca
Miro Dojkic <mdojkic@gmail.com>
2017-10-01 14:13:31 +0200

    cleanup

--- redux/connect.js
+++ redux/connect.js
@@ -2,20 +2,18 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import store from './store';
 
-export default function(selectors, actions) {
-  const mapStateToProps = state =>
-    Object.keys(selectors)
-      .map(selector => ({
-        [selector]: selectors[selector](state)
-      }))
-      .reduce((previous, current) => ({ ...previous, ...current }));
+const flattenObjects = (prev, curr) => ({ ...prev, ...curr });
 
-  const mapDispatchToProps = dispatch =>
-    Object.keys(actions)
-      .map(action => ({
-        [action]: actions[action](dispatch)
-      }))
-      .reduce((previous, current) => ({ ...previous, ...current }));
+const apply = functions => arg =>
+  Object.keys(functions)
+    .map(f => ({
+      [f]: functions[f](arg)
+    }))
+    .reduce(flattenObjects);
+
+export default function(selectors, actions) {
+  const mapStateToProps = apply(selectors);
+  const mapDispatchToProps = apply(actions);
 
   return component => connect(mapStateToProps, mapDispatchToProps)(component);
 }
