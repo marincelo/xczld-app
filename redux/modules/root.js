195282185d75b01e86e5e1f102cd3c46258a75ce
Miro Dojkic <mdojkic@gmail.com>
2017-10-02 23:10:47 +0200

    fix

--- redux/modules/root.js
+++ redux/modules/root.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import races from './races';
 
-const reducer = combineReducers({ races }));
+const reducer = combineReducers({ races });
 
 export default reducer;
