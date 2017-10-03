81eea62cce8f3483f8fb0ad3663681a46d1c82d9
Miro Dojkic <mdojkic@gmail.com>
2017-10-03 07:44:09 +0200

    missing semi

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const dataRetentionThreshold = 1000 * 60 * 10; // 10min
 const apiUrl = 'http://xczld.herokuapp.com/';
 
-export { dataRetentionThreshold, apiUrl }
+export { dataRetentionThreshold, apiUrl };
